4a120123acabd13460f9faa74ceb9f8f2d3b448f
muriithi-ian <imuriithiian@gmail.com>
2021-02-09 19:49:57 +0300

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+B
+src/pong/Pong.java,7\2\7277fc9af8344c3543714d16eb434edb019f2905
---
+++ src/pong/Pong.java
@@ -0,0 +1,18 @@
+package pong;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Pong extends Applet {
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+    public void update(Graphics g) {
+        paint(g);
+    }
+}
