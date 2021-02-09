2ebd5233deca070c4ff90e0af6a6790f742be1c8
muriithi-ian <imuriithiian@gmail.com>
2021-02-09 20:23:47 +0300

    setup applet

--- src/pong/Pong.java
+++ src/pong/Pong.java
@@ -2,17 +2,62 @@ package pong;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Pong extends Applet {
+public class Pong extends Applet implements Runnable, KeyListener {
+    final int WIDTH = 700, HEIGHT = 500;
+    Thread thread;
+
     public void init() {
+        this.resize(WIDTH, HEIGHT);
 
+        this.addKeyListener(this);
+
+        thread = new Thread(this);
+        thread.start();
     }
 
     public void paint(Graphics g) {
-
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
     }
 
     public void update(Graphics g) {
         paint(g);
+    }
+
+    public void run() {
+        for (; ; ) {
+
+
+
+            repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+
+        }
     }
 }
